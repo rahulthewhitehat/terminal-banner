7d62387ed913ca028df2405bf322e5424fa12f1f
Rahul Babu M P <106467094+rahulthewhitehat@users.noreply.github.com>
2022-08-01 08:33:21 +0530

    Add files via upload

---
+++ terminaledit2.py
@@ -0,0 +1,43 @@
+# This source code is made by me and it belongs to me (RahulTheWhiteHat, don't change noob !) But, it's a open source and you can use it...
+# Contact me using my instagram @archonofdreams_ for any enquiries...
+import time
+print("                                                   I N I T I A L I Z I N G   S Y S T E M")
+print("                                                  ----------------------------------------")
+time.sleep(0.3)
+print(" Checking system resources ...")
+time.sleep(0.6)
+print(" Connecting to internet...")  #  made by RahulTheWhiteHat
+time.sleep(0.7)
+print(" Configuring TOR Service ...")
+time.sleep(0.8)
+print(" Authorizing User ...")
+time.sleep(0.6)
+print(" Making Secure Log in ...")
+time.sleep(0.6)
+print(" Connecting to the Server ...")
+time.sleep(0.5)
+print(" All Set! Getting you in...")
+time.sleep(0.6)
+print(" Launching your system ...")
+time.sleep(0.8)
+
+print("                                                       W E L C O M E    M A S T E R ")          #  made by RahulTheWhiteHat
+print("                                                      ------------------------------    ")
+
+from rich.progress import Progress
+from rich import align
+
+ 
+with Progress() as progress:
+    task1 = progress.add_task("[red]ACTIVATING @ARCHONOFDREAMS_ ...", total=1000)
+    print("                                                                                               --Made by RahulTheWhiteHat")
+    #task2 = progress.add_task("[green]Final Processing...", total=500) -- optional
+    #task3 = progress.add_task("[cyan]Cooking...", total=1000) -- optional
+
+
+    while not progress.finished:
+     
+        progress.update(task1, advance=7)
+#       progress.update(task2, advance=2.5)           #  made by RahulTheWhiteHat
+#       progress.update(task3, advance=2)
+        time.sleep(0.02)
